b57731a573f6a8ec7c4d729c41e17b016af28a8f
DOG-BREAD <samueljacobsbuisness@gmail.com>
2023-09-28 13:52:59 -0500

    start of gui

--- gui/mainGui.py
+++ gui/mainGui.py
@@ -1,3 +1,10 @@
-import tkinter as tk
+from tkinter import *
 
-window = tk.Tk()
+root = Tk()
+root.title('Port Scanner')
+
+# Adjust size of the window
+root.geometry("400x400")
+root.configure(bg="#DC143C")
+
+root.mainloop()
